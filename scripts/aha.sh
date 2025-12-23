b3e2057e8b7e95d29bc12f283505e14518ef2ae2
Aran Varun <vir@tue.su>
2025-12-23 18:09:35 +0300

    workshop 2

--- scripts/aha.sh
+++ scripts/aha.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+INP=$1
+
 function print_qr_url () {
     echo
     echo $1
-    echo
     qrcode-terminal $1
-    echo
 }
 
-
-print_qr_url "https://ahaslides.com/HPMTJ"
-
-print_qr_url "https://github.com/dabevlohn/yp-course"
+case $INP in
+    1)
+        print_qr_url "https://ahaslides.com/HPMTJ"
+        ;;
+    2)
+        print_qr_url "https://github.com/dabevlohn/yp-course"
+        ;;
+esac
