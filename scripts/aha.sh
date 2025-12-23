0d2fb007f0c89b442d093831bb915d1493986dbd
Aran Varun <vir@tue.su>
2025-12-23 18:04:35 +0300

    workshop 2

--- scripts/aha.sh
+++ scripts/aha.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-qrcode-terminal https://ahaslides.com/HPMTJ
+function print_qr_url () {
+    echo
+    echo $1
+    echo
+    qrcode-terminal $1
+    echo
+}
+
+
+print_qr_url "https://ahaslides.com/HPMTJ"
+
+print_qr_url "https://github.com/dabevlohn/yp-course"
